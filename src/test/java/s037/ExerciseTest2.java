a9df2ae3dfb8d53bedd94b97db033e6cddb5a8f1
FCRA <fcrargenziano@gmail.com>
2020-03-17 15:38:05 +0100

    primo test scritto

---
+++ src/test/java/s037/ExerciseTest2.java
@@ -0,0 +1,20 @@
+package s037;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
+class ExerciseTest2 {
+
+	@Test
+	void testCesareMethod() {
+		String result = Exercise.cesareMethod("CIAO", 1);
+		
+		assertThat(result, is("DJBP"));
+	
+		
+	}
+
+}
