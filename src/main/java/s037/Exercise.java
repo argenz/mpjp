0a1ca10a2c84dc9dac9c02b9c47894f6e9206b26
FCRA <fcrargenziano@gmail.com>
2020-03-17 15:36:34 +0100

    Esercizio: Metodo di Cesare Encryption

--- src/main/java/s037/Exercise.java
+++ src/main/java/s037/Exercise.java
@@ -2,16 +2,65 @@ package s037;
 
 public class Exercise {
     final static int GAP = 'a' - 'A';  // final? Costante  
+    final static int MIDDLE = 6;
     
     public static void main(String[] args) {// scope == { ... }
-        exercise("Bob");
-        exercise("ciAo");
+        //exercise("Bob");
+        //exercise("ciAo");
 
-        System.out.println("toUpper(): " + toUpper("Bob"));
-        System.out.println("toUpper2(): " + toUpper2("Bob"));
-        System.out.println("toUpper3(): " + toUpper3("Bob"));
-        System.out.println("toUpper4(): " + toUpper4("Bob"));
+        //System.out.println("toUpper(): " + toUpper("Bob"));
+        //System.out.println("toUpper2(): " + toUpper2("Bob"));
+        //System.out.println("toUpper3(): " + toUpper3("Bob"));
+        //System.out.println("toUpper4(): " + toUpper4("Bob"));
+        
+        // Main 
+        String mes = "ZOO";
+        System.out.println("Ceasar encoding of " + mes + " is: " + cesareMethod(mes, 2));
+        System.out.println("Ceasar encoding of " + mes + " is: " + cesareMethod(mes, -2));
+        System.out.println("Ceasar encoding of " + mes + " is: " + cesareMethod(mes, 25)); //check this one
+        System.out.println("Ceasar encoding of " + mes + " is: " + cesareMethod(mes, 25)); //check this one
+        System.out.println("Ceasar encoding of " + mes + " is: " + cesareMethod(mes, 0));
+        System.out.println("Ceasar encoding of " + null + " is: " + cesareMethod(null, 2));
+        }
+    
+    	// Method
+    
+    public static String cesareMethod(String s, int shift) {
+    	 if (s == null || s.isEmpty()) {
+             return s;
+         }
+    	 if (shift==0 ) {
+    		 return s;
+    	 }
+    	 while(shift>=25){   //non necessario 
+    		 shift = shift%25;
+    	 }
+    	 StringBuilder sb = new StringBuilder(s.length());
+    	 
+         for (int i = 0; i < s.length(); i++) {
+        	 
+        	 char cur = s.charAt(i);
+        	 
+        	 if ((cur >= 'A' && cur < 'Z')||(cur >= 'a' && cur < 'z')){
+        			 sb.append((char)(cur + shift));	 
+        	 }
+        	 else if(cur == 'Z' || cur == 'z'){
+        		 if (shift>0) {
+        			 sb.append((char)(cur - GAP + MIDDLE + shift));
+        		 }
+        		 else if (shift<0) { 
+					 sb.append((char)(cur + shift)); }
+        	 }
+        	 else{
+        		 return "Error! the word must contain letters only.";	
+        	 }	 
+         }
+    return sb.toString();	
     }
+    
+    
+    
+    
 
     public static String toUpper(String s) {
         if (s == null || s.isEmpty()) {
