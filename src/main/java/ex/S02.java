94dc95fb8da25123c0ba6be7d9a9a1c391212eae
FCRA <fcrargenziano@gmail.com>
2020-03-17 21:29:31 +0100

    esercizi completati

--- src/main/java/ex/S02.java
+++ src/main/java/ex/S02.java
@@ -1,6 +1,12 @@
 package ex;
 
 public class S02 {
+	public static void main(String[] args) {
+		
+		System.out.println(S02.distance(1, 10, 2, 9));
+		System.out.println(S02.distance(0, 3.55, 0, 3.55));
+		System.out.println(S02.score(3.55, 3.55));
+	}
 	/**
 	 * Average speed
 	 * 
@@ -9,8 +15,10 @@ public class S02 {
 	 * @return speed in meters per second
 	 */
 	public static double speed(double distance, double time) {
-		// TODO
-		return 0;
+		
+		double meanSpeed = distance/time;
+		
+		return meanSpeed;
 	}
 
 	/**
@@ -22,22 +30,29 @@ public class S02 {
 	 * @param y1 second point y
 	 * @return distance
 	 */
-	public static double distance(int x0, int y0, int x1, int y1) {
-		// TODO
-		return 0;
+	
+	public static double distance(double x0, double y0, double x1, double y1) {
+		
+		double dis2 = Math.pow((x0-x1),2) + Math.pow((y0-y1),2);
+		double dis  = Math.sqrt(dis2);
+		
+		return dis;
 	}
 
 	/**
 	 * Engine capacity
 	 * 
-	 * @param bore   in mm
-	 * @param stroke in mm
+	 * @param bore   in mm diameter
+	 * @param stroke in mm lunghezza
 	 * @param nr     number of cylinders
 	 * @return the engine capacity in cm^3
 	 */
-	public static double engineCapacity(double bore, double stroke, int nr) {
-		// TODO
-		return 0;
+	public static double engineCapacity(double bore, double stroke, double nr) {
+		
+		double radius = bore/20;
+		double vol = Math.pow(radius, 2)*Math.PI*stroke/10;
+		double capacity = vol*nr;
+		return capacity;
 	}
 
 	/**
@@ -47,8 +62,17 @@ public class S02 {
 	 * @return sum of digits
 	 */
 	public static int digitSum(int value) {
-		// TODO
-		return 0;
+		value = Math.abs(value);
+	
+		int sum = 0;
+		
+		while (value > 0) {
+			int digit = value%10;
+			value = value/10;
+			sum = sum + digit;
+		}
+		
+		return sum ;
 	}
 
 	/**
@@ -60,7 +84,22 @@ public class S02 {
 	 * @return
 	 */
 	public static int score(double x, double y) {
-		// TODO
-		return 0;
+		
+		//int points = 0;
+		
+		double dist = S02.distance(0, 0, x, y);
+		
+		if (dist <= 1) {
+			return 10;	
+			
+		}else if (dist > 1 && dist <= 5) {
+			return 5;
+			
+		}else if (dist > 5 && dist <= 10) {
+			return 1;
+			
+		}else {
+			return 0;
+		}
 	}
 }
